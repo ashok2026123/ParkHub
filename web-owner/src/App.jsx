b326686c156fdf1f844b43d7ebe84fb70f4c7033
HelloPay Admin <hellopay@admin.com>
2026-06-28 23:43:14 +0530

    feat(owner): auto switch to Earnings Overview tab when user books cash on delivery and OTP modal pops up

--- web-owner/src/App.jsx
+++ web-owner/src/App.jsx
@@ -299,6 +299,7 @@ export default function App() {
         
         setSelectedBookingForOtp(notifyBooking);
         setShowBookingOtpModal(true);
+        setCurrentTab('overview'); // <-- Switch to Earnings Overview tab automatically
         // Play notification sound
         try {
           const audio = new Audio('https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3');
